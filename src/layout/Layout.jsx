0f3c335321cdff122d55f4538253b35b2efc05b1
listerineh <jsalvarez4@espe.edu.ec>
2022-06-14 23:15:35 -0500

    Feature: Create the initial front of the page

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,10 +1,32 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, Link, useLocation } from 'react-router-dom'
 
 function Layout() {
+
+  const location = useLocation()
+  const currentURL = location.pathname
+
   return (
-    <div>
-      <h1>Layout</h1>
-      <Outlet />
+    <div className='md:flex md:min-h-screen '>
+      
+      <div className='md:w-1/4 bg-red-900 px-5 py-10'>
+        <h2 className='text-5xl md:text-2xl lg:text-3xl font-black text-center text-white'>
+          Clients Administrator
+        </h2>
+        <nav className='mt-10 '>
+          <Link 
+            to='/clients' 
+            className={`${currentURL === '/clients' ? 'text-red-300' : 'text-white'}  text-2xl block mt-2 hover:text-red-200`}
+          >Clients</Link>
+          <Link 
+            to='/clients/new' 
+            className={`${currentURL === '/clients/new' ? 'text-red-300' : 'text-white'}  text-2xl block mt-2 hover:text-red-200`}
+          >New Client</Link>
+        </nav>
+      </div>
+      <div className='md:w-3/4 p-10'>
+        <Outlet />
+      </div>
+
     </div>
   )
 }
--- src/pages/NewClient.jsx
+++ src/pages/NewClient.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import Form from '../components/Form'
 
 function NewClient() {
   return (
-    <div>
-      <h1>New Clients</h1>
-    </div>
+    <>
+      <h1 className="font-black text-4xl text-red-900">New Client</h1>
+      <p className='mt-3'>Fill this fields to register a new client!</p>
+      <Form />
+    </>
   )
 }
 
